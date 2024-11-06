7d554366c7ef231d47f35570ee2c38cd706a32ee
Tiarintsoa <rihantiana000@gmail.com>
2024-11-06 10:58:21 +0300

    Initial commit

---
+++ database/schema.sql
@@ -0,0 +1,59 @@
+CREATE TABLE type_produit(
+                             id_type_produit SERIAL,
+                             nom_type_produit VARCHAR(50)  NOT NULL,
+                             PRIMARY KEY(id_type_produit)
+);
+
+CREATE TABLE produit(
+                        id_produit SERIAL,
+                        nom_produit VARCHAR(255)  NOT NULL,
+                        longueur NUMERIC(15,2)   NOT NULL,
+                        largeur NUMERIC(15,2)   NOT NULL,
+                        hauteur NUMERIC(15,2)   NOT NULL,
+                        id_type_produit INTEGER NOT NULL,
+                        PRIMARY KEY(id_produit),
+                        UNIQUE(nom_produit),
+                        FOREIGN KEY(id_type_produit) REFERENCES type_produit(id_type_produit)
+);
+
+CREATE TABLE mouvement_stock(
+                                id_mouvement_stock SERIAL,
+                                quantite_entree INTEGER NOT NULL,
+                                quantite_sortie NUMERIC(15,2)   NOT NULL,
+                                date_heure_mouvement TIMESTAMP NOT NULL DEFAULT NOW(),
+                                id_produit INTEGER NOT NULL,
+                                PRIMARY KEY(id_mouvement_stock),
+                                FOREIGN KEY(id_produit) REFERENCES produit(id_produit)
+);
+
+CREATE TABLE bloc(
+                     id_bloc SERIAL,
+                     prix_production MONEY NOT NULL,
+                     id_origine INTEGER,
+                     id_produit INTEGER NOT NULL,
+                     PRIMARY KEY(id_bloc),
+                     UNIQUE(id_produit),
+                     FOREIGN KEY(id_origine) REFERENCES bloc(id_bloc),
+                     FOREIGN KEY(id_produit) REFERENCES produit(id_produit)
+);
+
+CREATE TABLE forme_usuelle(
+                              id_forme_usuelle SERIAL,
+                              prix_vente MONEY NOT NULL,
+                              id_produit INTEGER NOT NULL,
+                              PRIMARY KEY(id_forme_usuelle),
+                              UNIQUE(id_produit),
+                              FOREIGN KEY(id_produit) REFERENCES produit(id_produit)
+);
+
+CREATE TABLE etat_stock(
+                           id_etat_stock SERIAL,
+                           quantite INTEGER NOT NULL,
+                           prix_production MONEY NOT NULL,
+                           date_heure_insertion TIMESTAMP NOT NULL DEFAULT NOW(),
+                           id_origine INTEGER,
+                           id_produit INTEGER NOT NULL,
+                           PRIMARY KEY(id_etat_stock),
+                           FOREIGN KEY(id_origine) REFERENCES bloc(id_bloc),
+                           FOREIGN KEY(id_produit) REFERENCES produit(id_produit)
+);
---
+++ src/main/java/com/tiarintsoa/foam/FoamApplication.java
@@ -0,0 +1,13 @@
+package com.tiarintsoa.foam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoamApplication.class, args);
+	}
+
+}
