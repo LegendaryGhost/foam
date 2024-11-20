eab36059d409d17463b3a5baaab84cc1c3668037
Tiarintsoa <rihantiana000@gmail.com>
2024-11-20 17:16:40 +0300

    Data + triggers fix

--- database/data_alea.sql
+++ database/data_alea.sql
@@ -1,24 +1,45 @@
-INSERT INTO type_produit (nom_type_produit) VALUES
+-- Types produit
+INSERT INTO type_produit(nom_type_produit) VALUES
                                                 ('bloc'),
                                                 ('forme usuelle');
 
+
+
+-- Machines
+INSERT INTO machine(nom_machine) VALUES
+                                     ('M1'),
+                                     ('M2'),
+                                     ('M3'),
+                                     ('M4');
+
+
+
 -- Formes usuelles
-INSERT INTO produit(nom_produit, longueur, largeur, hauteur, id_type_produit) VALUES
-                                                                                  ('U1', 16, 4, 2, 2),
-                                                                                  ('U2', 10, 7, 1, 2),
-                                                                                  ('U3', 5, 1, 1, 2);
+INSERT INTO article(nom_article) VALUES
+                                     ('U1'),
+                                     ('U2'),
+                                     ('U3');
+INSERT INTO produit(id_article, longueur, largeur, hauteur, id_type_produit) VALUES
+                                                                                  (1, 16, 4, 2, 2),
+                                                                                  (2, 10, 7, 1, 2),
+                                                                                  (3, 5, 1, 1, 2);
 INSERT INTO forme_usuelle(prix_vente, id_produit) VALUES
                                                       (20000, 1),
                                                       (12000, 2),
                                                       (600, 3);
 
+
 -- Blocs
-INSERT INTO produit(nom_produit, longueur, largeur, hauteur, id_type_produit) VALUES
-                                                                                  ('Bloc 1',  100, 20, 10, 1),
-                                                                                  ('Bloc 2',  100, 40, 10, 1);
-INSERT INTO bloc(prix_production, id_produit) VALUES
-                                                              (2000000, 4),
-                                                              (3000000, 5);
-INSERT INTO etat_stock(quantite, prix_production, id_produit) VALUES
+INSERT INTO article(nom_article) VALUES
+                                     ('Bloc 1'),
+                                     ('Bloc 2');
+
+INSERT INTO produit(id_article, longueur, largeur, hauteur, id_type_produit) VALUES
+                                                                                  (4,  100, 20, 10, 1),
+                                                                                  (5,  100, 40, 10, 1);
+INSERT INTO bloc(prix_production, id_machine, id_produit) VALUES
+                                                              (2000000, 1, 4),
+                                                              (3000000, 2, 5);
+INSERT INTO etat_stock(quantite, prix_production, id_article) VALUES
                                                                               (1, 2000000, 4),
                                                                               (1, 3000000, 5);
--- database/trigger.sql
+++ database/trigger.sql
@@ -1,21 +1,19 @@
 -- Fonction pour le trigger qui gère l'insertion dans mouvement_stock
 CREATE OR REPLACE FUNCTION insert_mouvement_stock()
-    RETURNS TRIGGER AS $$
+    RETURNS TRIGGER AS
+$$
 BEGIN
     -- Insérer un nouveau mouvement de stock
-    INSERT INTO mouvement_stock (
-        quantite_entree,
-        quantite_sortie,
-        prix_vente,
-        date_heure_mouvement,
-        id_article
-    ) VALUES (
-                 NEW.quantite,        -- La quantité initiale devient la quantité d'entrée
-                 0,                   -- La quantité de sortie est initialisée à 0
-                 NEW.prix_vente,
-                 NEW.date_heure_insertion,
-                 NEW.id_produit
-             );
+    INSERT INTO mouvement_stock (quantite_entree,
+                                 quantite_sortie,
+                                 prix_vente,
+                                 date_heure_mouvement,
+                                 id_article)
+    VALUES (NEW.quantite, -- La quantité initiale devient la quantité d'entrée
+            0, -- La quantité de sortie est initialisée à 0
+            NEW.prix_vente,
+            NEW.date_heure_insertion,
+            NEW.id_article);
 
     RETURN NEW;
 END;
@@ -23,46 +21,44 @@ $$ LANGUAGE plpgsql;
 
 -- Créer le trigger qui s'exécute après l'insertion dans etat_stock
 CREATE TRIGGER after_insert_etat_stock
-    AFTER INSERT ON etat_stock
+    AFTER INSERT
+    ON etat_stock
     FOR EACH ROW
-    EXECUTE FUNCTION insert_mouvement_stock();
+EXECUTE FUNCTION insert_mouvement_stock();
 
 
 -- Fonction de déclenchement modifiée
 CREATE OR REPLACE FUNCTION insert_mouvement_stock_on_update()
-    RETURNS TRIGGER AS $$
+    RETURNS TRIGGER AS
+$$
 BEGIN
     -- Cas où la quantité a diminué
     IF NEW.quantite < OLD.quantite THEN
-        INSERT INTO mouvement_stock (
-            quantite_entree,
-            quantite_sortie,
-            prix_vente,
-            date_heure_mouvement,
-            id_article
-        ) VALUES (
-                     0,                             -- Aucune entrée
-                     OLD.quantite - NEW.quantite,   -- Quantité de sortie
-                     OLD.prix_vente,
-                     NOW(),                         -- Date et heure actuelles
-                     NEW.id_produit                 -- ID du produit
-                 );
+        INSERT INTO mouvement_stock (quantite_entree,
+                                     quantite_sortie,
+                                     prix_vente,
+                                     date_heure_mouvement,
+                                     id_article)
+        VALUES (0, -- Aucune entrée
+                OLD.quantite - NEW.quantite, -- Quantité de sortie
+                OLD.prix_vente,
+                NOW(), -- Date et heure actuelles
+                NEW.id_article -- ID du produit
+               );
 
         -- Cas où la quantité a augmenté
     ELSIF NEW.quantite > OLD.quantite THEN
-        INSERT INTO mouvement_stock (
-            quantite_entree,
-            quantite_sortie,
-            prix_vente,
-            date_heure_mouvement,
-            id_article
-        ) VALUES (
-                     NEW.quantite - OLD.quantite,   -- Quantité d'entrée
-                     0,                             -- Aucune sortie
-                     OLD.prix_vente,
-                     NOW(),                         -- Date et heure actuelles
-                     NEW.id_produit                 -- ID du produit
-                 );
+        INSERT INTO mouvement_stock (quantite_entree,
+                                     quantite_sortie,
+                                     prix_vente,
+                                     date_heure_mouvement,
+                                     id_article)
+        VALUES (NEW.quantite - OLD.quantite, -- Quantité d'entrée
+                0, -- Aucune sortie
+                OLD.prix_vente,
+                NOW(), -- Date et heure actuelles
+                NEW.id_article -- ID du produit
+               );
     END IF;
 
     RETURN NEW;
@@ -71,14 +67,16 @@ $$ LANGUAGE plpgsql;
 
 -- Trigger pour la table etat_stock
 CREATE TRIGGER trigger_update_etat_stock
-    AFTER UPDATE ON etat_stock
+    AFTER UPDATE
+    ON etat_stock
     FOR EACH ROW
     WHEN (NEW.quantite != OLD.quantite) -- Condition pour tout changement de quantité
 EXECUTE FUNCTION insert_mouvement_stock_on_update();
 
 
 CREATE OR REPLACE FUNCTION update_prix_production_dependants()
-    RETURNS TRIGGER AS $$
+    RETURNS TRIGGER AS
+$$
 DECLARE
     ratio NUMERIC;
 BEGIN
@@ -100,7 +98,8 @@ END;
 $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER trigger_update_prix_production
-    AFTER UPDATE OF prix_production ON bloc
+    AFTER UPDATE OF prix_production
+    ON bloc
     FOR EACH ROW
     WHEN (OLD.prix_production IS DISTINCT FROM NEW.prix_production)
 EXECUTE FUNCTION update_prix_production_dependants();
